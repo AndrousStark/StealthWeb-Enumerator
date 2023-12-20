eefc5f729b685be6a665a4008063bf856238585f
AndrousStark <55905689+AndrousStark@users.noreply.github.com>
2023-12-21 00:31:56 +0530

    Create main.py

---
+++ core/StealthWeb/main.py
@@ -0,0 +1,31 @@
+
+class TerminalColor:
+	Black = '\033[30m'
+	Red = '\033[31m'
+	Green = '\033[32m'
+	Orange = '\033[33m'
+	Blue = '\033[34m'
+	Purple = '\033[35m'
+	Reset = '\033[0m'
+	Cyan = '\033[36m'
+	LightGrey = '\033[37m'
+	DarkGrey = '\033[90m'
+	LightRed = '\033[91m'
+	LightGreen = '\033[92m'
+	Yellow = '\033[93m'
+	LightBlue = '\033[94m'
+	Pink = '\033[95m'
+	LightCyan = '\033[96m'
+
+class Banner:
+	def StealthWebBanner():
+		print(f""" 
+                                                                                      
+              ░█▀█░█▀█░█▀▄░█▀▄░█▀█░█░█░█▀▀░░░░░█▀▀░▀█▀░█▀█░█▀▄░█░█
+              ░█▀█░█░█░█░█░█▀▄░█░█░█░█░▀▀█░░░░░▀▀█░░█░░█▀█░█▀▄░█▀▄
+              ░▀░▀░▀░▀░▀▀░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░░▀░░▀░▀░▀░▀░▀░▀
+
+Coded by:{TerminalColor.Red} Aniruddh Atrey{TerminalColor.Reset} Email:{TerminalColor.Purple} aniruddh.atrey111101@gmail.com {TerminalColor.Reset}
+LinkedIn: {TerminalColor.Red}https://www.linkedin.com/in/aniruddhatrey/{TerminalColor.Reset}
+ [{TerminalColor.Green}Welcome to StealthWeb v1.0{TerminalColor.Reset}]
+======================================================================================================""")
